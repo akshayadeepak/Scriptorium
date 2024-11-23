5dff2c76f1bb44f0d94a7a352dd2e1f133ac289b
mumark <mark.mu@mail.utoronto.ca>
2024-11-23 10:18:26 -0500

    temp

--- PP2/pages/profile.tsx
+++ PP2/pages/profile.tsx
@@ -31,7 +31,6 @@ const Profile: React.FC = () => {
     const [successMessage, setSuccessMessage] = useState('');
     const [editModal, setEditModal] = useState<CodeTemplate | null>(null);
 
-
     useEffect(() => {
         const fetchUserData = async () => {
             try {
@@ -84,65 +83,59 @@ const Profile: React.FC = () => {
     if (!profile) return <div>Loading...</div>;
 
     const handleEditButtonClick = (template: CodeTemplate) => {
-        setEditModal(template); // Populate modal with selected template data
-      };
-      
-      const handleEditTemplate = async () => {
+        setEditModal(template);
+    };
+
+    const handleEditTemplate = async () => {
         if (!editModal) return;
         try {
-          const token = localStorage.getItem('token');
-      
-          if (!token) {
-            setError('Authorization token is missing. Please log in.');
-            return;
-          }
-          console.log(editModal.tags)
-          const response = await fetch(`/api/code/template?id=${editModal.id}`, {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`,
-            },
-            body: JSON.stringify({
-              id: editModal.id,
-              title: editModal.title,
-              explanation: editModal.explanation,
-            //   tags: editModal.tags.split(',').map((tagName) => tagName.trim()),
-              language: editModal.language,
-              content: editModal.content,
-            }),
-          });
-      
-          if (response.ok) {
-            const updatedTemplate = await response.json();
-            setTemplates((prev) =>
-              prev.map((template) =>
-                template.id === updatedTemplate.id ? updatedTemplate : template
-              )
-            );
-            setEditModal(null);
-            setSuccessMessage('Template updated successfully!');
-          } else {
-            setError('Failed to update template.');
-          }
+            const token = localStorage.getItem('token');
+
+            if (!token) {
+                setError('Authorization token is missing. Please log in.');
+                return;
+            }
+
+            const response = await fetch(`/api/code/template?id=${editModal.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    id: editModal.id,
+                    title: editModal.title,
+                    explanation: editModal.explanation,
+                    tags: editModal.tags, // Correctly include the tags array
+                    language: editModal.language,
+                    content: editModal.content,
+                }),
+            });
+
+            if (response.ok) {
+                const updatedTemplate = await response.json();
+                setTemplates((prev) =>
+                    prev.map((template) =>
+                        template.id === updatedTemplate.id ? updatedTemplate : template
+                    )
+                );
+                setEditModal(null);
+                setSuccessMessage('Template updated successfully!');
+            } else {
+                setError('Failed to update template.');
+            }
         } catch (error) {
-          console.error('Error updating template:', error);
-          setError('Failed to update template.');
+            console.error('Error updating template:', error);
+            setError('Failed to update template.');
         }
-      };
+    };
 
     return (
         <AuthGuard>
             <div className="w-full min-h-screen bg-cover bg-center bg-no-repeat bg-fixed" 
-                 style={{ 
-                     backgroundImage: "url('/banners/index.png')", 
-                     position: 'relative' 
-                 }}>
-                <div className="absolute inset-0" style={{ 
-                    backgroundColor: 'rgba(255, 255, 255, 0.7)', 
-                    backdropFilter: 'blur(5px)', 
-                    zIndex: 0
-                }} />
+                 style={{ backgroundImage: "url('/banners/index.png')", position: 'relative' }}>
+                <div className="absolute inset-0" 
+                     style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)', backdropFilter: 'blur(5px)', zIndex: 0 }} />
                 <div style={{ position: 'relative', zIndex: 1 }}>
                     <Navbar />
                     <div className="flex w-full p-4">
@@ -163,29 +156,28 @@ const Profile: React.FC = () => {
                                     <span className="text-gray-600 italic">Avatar not set</span>
                                 </div>
                             )}
-
                             <div className="mb-8">
-                                <div className="p-4 border-b border-gray-300 transition hover:bg-gray-200">
+                                <div className="p-4 border-b border-gray-300">
                                     <strong>Username:</strong> {profile.username}
                                 </div>
-                                <div className="p-4 border-b border-gray-300 transition hover:bg-gray-200">
+                                <div className="p-4 border-b border-gray-300">
                                     <strong>Email:</strong> {profile.email}
                                 </div>
-                                <div className="p-4 border-b border-gray-300 transition hover:bg-gray-200">
+                                <div className="p-4 border-b border-gray-300">
                                     <strong>Phone:</strong> {profile.phoneNumber || 'Not set'}
                                 </div>
-                                <div className="p-4 border-b border-gray-300 transition hover:bg-gray-200">
+                                <div className="p-4 border-b border-gray-300">
                                     <strong>First name:</strong> {profile.firstName || 'Not set'}
                                 </div>
-                                <div className="p-4 border-b border-gray-300 transition hover:bg-gray-200">
+                                <div className="p-4 border-b border-gray-300">
                                     <strong>Last name:</strong> {profile.lastName || 'Not set'}
                                 </div>
                                 <div className="flex justify-center">
                                     <button 
-                                            onClick={() => router.push('/edit-profile')}
-                                            className="px-6 py-3 m-7 bg-blue-500 text-white rounded hover:bg-blue-600"
-                                        >
-                                            Edit Profile
+                                        onClick={() => router.push('/edit-profile')}
+                                        className="px-6 py-3 m-7 bg-blue-500 text-white rounded hover:bg-blue-600"
+                                    >
+                                        Edit Profile
                                     </button>
                                 </div>
                             </div>
@@ -201,31 +193,27 @@ const Profile: React.FC = () => {
                                     ) : (
                                         <ul className="list-none p-0">
                                             {templates.map(template => (
-                                                <li key={template.id} className="mb-6 p-4 border border-gray-300 rounded-lg transition hover:shadow-lg">
-                                                    <h4 className="text-lg font-bold mb-2 text-gray-800">
-                                                        {template.title} <span className="text-gray-600 text-sm">{`(${template.language})`}</span>
+                                                <li key={template.id} className="mb-6 p-4 border rounded-lg">
+                                                    <h4 className="text-lg font-bold text-gray-800 mb-2">
+                                                        {template.title} <span className="text-sm text-gray-600">{`(${template.language})`}</span>
                                                     </h4>
-                                                    <pre className="bg-gray-200 p-2 rounded overflow-x-auto">
+                                                    <pre className="bg-gray-200 p-2 rounded">
                                                         <code>{template.content}</code>
                                                     </pre>
                                                     {template.explanation && (
                                                         <p className="mt-2 text-gray-600">{template.explanation}</p>
                                                     )}
                                                     <div className="flex flex-wrap gap-2 mt-2">
-                                                        {(template.tags || []).map(tag => (
-                                                            <span key={tag.name} className="text-blue-500 text-sm">
-                                                                #{tag.name}
-                                                            </span>
+                                                        {template.tags.map(tag => (
+                                                            <span key={tag.name} className="text-blue-500 text-sm">#{tag.name}</span>
                                                         ))}
                                                     </div>
-                                                    <div className="flex gap-4 items-center mt-4 pt-4 border-t border-gray-100">
-                                                        <button
-                                                            onClick={() => handleEditButtonClick(template)}
-                                                            className="text-sm text-gray-500 hover:text-[#1da1f2] transition-colors"
-                                                        >
-                                                            Edit
-                                                        </button>
-                                                    </div>
+                                                    <button
+                                                        onClick={() => handleEditButtonClick(template)}
+                                                        className="text-sm text-gray-500 mt-4"
+                                                    >
+                                                        Edit
+                                                    </button>
                                                 </li>
                                             ))}
                                         </ul>
@@ -234,80 +222,72 @@ const Profile: React.FC = () => {
                             </div>
                         </div>
                     </div>
-                    {/* <div className="bg-white bg-opacity-80 rounded-b-lg shadow p-4 mt-0">
-                        <div className="flex justify-center gap-4">
-                            <button 
-                                onClick={() => router.push('/edit-profile')}
-                                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                            >
-                                Edit Profile
-                            </button>
-                        </div>
-                    </div> */}
-                    {/* Editing a Template */}
                     {editModal && (
-                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                        <div className="bg-white p-6 rounded-lg shadow-lg w-3/4 max-w-lg">
-                        <h2 className="text-xl font-semibold mb-4">Edit Template</h2>
-                        <input
-                            type="text"
-                            placeholder="Title"
-                            value={editModal.title}
-                            onChange={(e) =>
-                            setEditModal({ ...editModal, title: e.target.value })
-                            }
-                            className="w-full mb-4 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                        />
-                        <textarea
-                            placeholder="Explanation"
-                            value={editModal.explanation}
-                            onChange={(e) =>
-                            setEditModal({ ...editModal, explanation: e.target.value })
-                            }
-                            className="w-full mb-4 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Tags (comma-separated)"
-                            value={editModal.tags.map((tag) => tag.name).join(',')}
-                            onChange={(e) =>
-                            setEditModal({...editModal, tags: e.target.value})
-                            }
-                            className="w-full mb-4 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Language"
-                            value={editModal.language}
-                            onChange={(e) =>
-                            setEditModal({ ...editModal, language: e.target.value })
-                            }
-                            className="w-full mb-4 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-                        />
-                        <textarea
-                            placeholder="Code Content"
-                            value={editModal.content}
-                            onChange={(e) =>
-                            setEditModal({ ...editModal, content: e.target.value })
-                            }
-                            className="w-full mb-4 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400 h-24"
-                        />
-                        <div className="flex justify-end">
-                            <button
-                            onClick={() => setEditModal(null)}
-                            className="bg-gray-300 px-4 py-2 rounded mr-2"
-                            >
-                            Cancel
-                            </button>
-                            <button
-                            onClick={handleEditTemplate}
-                            className="bg-blue-500 text-white px-4 py-2 rounded"
-                            >
-                            Save Changes
-                            </button>
+                        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                            <div className="bg-white p-6 rounded-lg shadow-lg w-3/4 max-w-lg">
+                                <h2 className="text-xl font-semibold mb-4">Edit Template</h2>
+                                <input
+                                    type="text"
+                                    placeholder="Title"
+                                    value={editModal.title}
+                                    onChange={(e) =>
+                                        setEditModal({ ...editModal, title: e.target.value })
+                                    }
+                                    className="w-full mb-4 p-2 border rounded"
+                                />
+                                <textarea
+                                    placeholder="Explanation"
+                                    value={editModal.explanation}
+                                    onChange={(e) =>
+                                        setEditModal({ ...editModal, explanation: e.target.value })
+                                    }
+                                    className="w-full mb-4 p-2 border rounded"
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="Tags (comma-separated)"
+                                    value={editModal.tags.map((tag) => tag.name).join(', ')}
+                                    onChange={(e) =>
+                                        setEditModal({
+                                            ...editModal,
+                                            tags: e.target.value.split(',').map((tagName) => ({ name: tagName.trim() })),
+                                        })
+                                    }
+                                    className="w-full mb-4 p-2 border rounded"
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="Language"
+                                    value={editModal.language}
+                                    onChange={(e) =>
+                                        setEditModal({ ...editModal, language: e.target.value })
+                                    }
+                                    className="w-full mb-4 p-2 border rounded"
+                                />
+                                <textarea
+                                    placeholder="Code Content"
+                                    value={editModal.content}
+                                    onChange={(e) =>
+                                        setEditModal({ ...editModal, content: e.target.value })
+                                    }
+                                    className="w-full mb-4 p-2 border rounded h-24"
+                                />
+                                <div className="flex justify-end">
+                                    <button
+                                        onClick={() => setEditModal(null)}
+                                        className="bg-gray-300 px-4 py-2 rounded mr-2"
+                                    >
+                                        Cancel
+                                    </button>
+                                    <button
+                                        onClick={handleEditTemplate}
+                                        className="bg-blue-500 text-white px-4 py-2 rounded"
+                                    >
+                                        Save Changes
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                        </div>
-                    </div>
                     )}
                 </div>
             </div>
